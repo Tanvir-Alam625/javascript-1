43a0b6c737c9034f99ecdcc1ad0b799f48b60813
tanveer <mdtanviralamht731@gmail.com>
2022-02-01 17:20:17 +0600

    swap file added

---
+++ swap.js
@@ -0,0 +1,14 @@
+var a = 45;
+var b = 35;
+console.log(a,b);
+// swaping 
+// var temporary = a;
+// a=b;
+// b=temporary;
+// console.log(a,b);
+
+
+
+// arry distructuring
+[a,b] = [b,a];
+ console.log(a,b);
